227ac3c7a4693037fbc635c23ba36facf9172416
Damian Axford <damian@axford.me.uk>
2015-09-27 21:56:37 +0100

    Joint test using Adafruit library

--- software/firmware/jointTest/jointTest.ino
+++ software/firmware/jointTest/jointTest.ino
@@ -19,23 +19,18 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
+// servo driver on default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
+// limits
 //#define SERVOMIN  150 
 //#define SERVOMAX  600 
 
 #define SERVOMIN  150 + 400 / 2 
 #define SERVOMAX  150 + 500 / 2
 
-uint16_t servoMins[6] = {350, 350, 350, 250, 350, 350};
-uint16_t servoMaxs[6] = {400, 400, 400, 300, 400, 400};
+uint16_t servoMins[6] = {300, 250, 250, 250, 350, 250};
+uint16_t servoMaxs[6] = {600, 500, 500, 300, 400, 500};
 
 
 // our servo # counter
@@ -43,27 +38,16 @@ uint8_t servonum = 0;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("16 channel Servo test!");
+  Serial.println("Quadruloop servo test!");
 
   pwm.begin();
   
-  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-}
+  pwm.setPWMFreq(60);  // 60 Hz updates
 
-// you can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= 60;   // 60 Hz
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000;
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
+  // set start positions
+  for (servonum = 0; servonum < 5; servonum++) {
+    pwm.setPWM(servonum, 0, servoMins[servonum]);
+  }
 }
 
 void loop() {
@@ -71,14 +55,13 @@ void loop() {
   Serial.println(servonum);
   for (uint16_t pulselen = servoMins[servonum]; pulselen < servoMaxs[servonum]; pulselen++) {
     pwm.setPWM(servonum, 0, pulselen);
-    Serial.println(pulselen);
+    delay(5);
   }
-  delay(500);
+  delay(250);
   for (uint16_t pulselen = servoMaxs[servonum]; pulselen > servoMins[servonum]; pulselen--) {
     pwm.setPWM(servonum, 0, pulselen);
-    Serial.println(pulselen);
+    delay(5);
   }
-  delay(500);
 
   servonum ++;
   if (servonum > 5) servonum = 0;
